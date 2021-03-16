a9c2cf320b0f9e0a83cc6487718cbe264dd76ccc
christianyabetaplay@outlook.com <christianyabetaplay@outlook.com>
2021-03-16 19:26:08 -0400

    Agregar archivos de proyecto.

---
+++ apiDoble/Controllers/DataController.cs
@@ -0,0 +1,44 @@
+﻿using apiDoble.Models;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiDoble.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+
+            string connectionString = "Endpoint=sb://yabetaholding.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=RypdtkeHAdSf8LxbIx2LWJoEbetcsuvzxMrp5mEs+gM=;EntityPath=cola1";
+            string queueName = "cola1";
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+
+    }
+}
